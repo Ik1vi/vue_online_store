a93fd047335c5fa79c099c7ee5af942517a050ce
Irene Veselova <ik1vikuz@gmail.com>
2021-05-26 14:13:37 +0300

    pagination

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
           <a href="#">{{ product.title }}</a>
         </h3>
 
-        <span class="catalog__price">{{product.price}} р</span>
+        <span class="catalog__price">{{ product.price }} р</span>
 
         <ul class="colors colors--black">
           <li class="colors__item">
@@ -40,61 +40,39 @@
         </ul>
       </li>
     </ul>
-
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a
-          class="pagination__link pagination__link--arrow pagination__link--disabled"
-          aria-label="Предыдущая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current"> 1 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 2 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 3 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 4 </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> ... </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#"> 10 </a>
-      </li>
-      <li class="pagination__item">
-        <a
-          class="pagination__link pagination__link--arrow"
-          href="#"
-          aria-label="Следующая страница"
-        >
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
   </section>
+
+  <BasePagination
+    v-model:page="page"
+    :count="countProducts"
+    :per-page="productsPerPage"
+  >
+  </BasePagination>
 </template>
 
 <script>
 import products from './data/products';
+import BasePagination from './components/BasePagination.vue';
 
 export default {
   name: 'App',
+  components: { BasePagination },
   data() {
     return {
       publicPath: process.env.BASE_URL,
-      products,
+      page: 1,
+      productsPerPage: 3,
     };
+  },
+
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 </script>
---
+++ src/components/BasePagination.vue
@@ -0,0 +1,81 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        href="#"
+        aria-label="Предыдущая страница"
+        :class="{'pagination__link--disabled': page === 1}"
+        :disabled="prevBtnIsDisabled"
+        @click.prevent="previousPage()"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNum of pages" :key="pageNum">
+      <a
+        class="pagination__link"
+        href="#"
+        :class="{'pagination__link--current': pageNum === page}"
+        @click.prevent="paginate(pageNum)"
+      >
+        {{pageNum}}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        href="#"
+        aria-label="Следующая страница"
+        :class="{'pagination__link--disabled': page === pages}"
+        :disabled="nextBtnIsDisabled"
+        @click.prevent="nextPage()"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['page', 'count', 'perPage'],
+  emits: ['update:page'],
+  data() {
+    return {
+      prevBtnIsDisabled: false,
+      nextBtnIsDisabled: false,
+    };
+  },
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.perPage);
+    },
+  },
+  methods: {
+    paginate(page) {
+      this.$emit('update:page', page);
+    },
+    nextPage() {
+      if (this.page === this.pages) {
+        this.nextBtnIsDisabled = true;
+      } else {
+        this.nextBtnIsDisabled = false;
+        this.$emit('update:page', this.page + 1);
+      }
+    },
+    previousPage() {
+      if (this.page === 1) {
+        this.prevBtnIsDisabled = true;
+      } else {
+        this.prevBtnIsDisabled = false;
+        this.$emit('update:page', this.page - 1);
+      }
+    },
+  },
+};
+</script>
