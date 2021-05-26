cea69f3702059c56e465305d2d63ed01604466ee
Irene Veselova <ik1vikuz@gmail.com>
2021-05-26 19:20:41 +0300

    components & keys

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,208 @@
 <template>
-  <section class="catalog">
-    <ul class="catalog__list">
-      <li class="catalog__item" v-for="(product, i) in products" :key="i">
-        <a class="catalog__pic" href="#">
-          <img :src="`${publicPath}${product.image}`" :alt="product.title" />
-        </a>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">Каталог</h1>
+      <span class="content__info"> 152 товара </span>
+    </div>
 
-        <h3 class="catalog__title">
-          <a href="#">{{ product.title }}</a>
-        </h3>
+    <div class="content__catalog">
+      <aside class="filter">
+        <h2 class="filter__title">Фильтры</h2>
 
-        <span class="catalog__price">{{ product.price }} р</span>
-
-        <ul class="colors colors--black">
-          <li class="colors__item">
-            <label class="colors__label">
-              <input
-                class="colors__radio sr-only"
-                type="radio"
-                name="color-1"
-                value="#73B6EA"
-                checked=""
-              />
-              <span class="colors__value" style="background-color: #73b6ea"> </span>
+        <form class="filter__form form" action="#" method="get">
+          <fieldset class="form__block">
+            <legend class="form__legend">Цена</legend>
+            <label class="form__label form__label--price">
+              <input class="form__input" type="text" name="min-price" value="0" />
+              <span class="form__value">От</span>
             </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000" />
-              <span class="colors__value" style="background-color: #8be000"> </span>
+            <label class="form__label form__label--price">
+              <input class="form__input" type="text" name="max-price" value="12345" />
+              <span class="form__value">До</span>
             </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#222" />
-              <span class="colors__value" style="background-color: #222"> </span>
-            </label>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </section>
+          </fieldset>
 
-  <BasePagination
-    v-model:page="page"
-    :count="countProducts"
-    :per-page="productsPerPage"
-  >
-  </BasePagination>
+          <fieldset class="form__block">
+            <legend class="form__legend">Категория</legend>
+            <label class="form__label form__label--select">
+              <select class="form__select" type="text" name="category">
+                <option value="value1">Все категории</option>
+                <option value="value2">Зубные щетки</option>
+                <option value="value3">Телефоны</option>
+                <option value="value4">Спортинвентарь</option>
+              </select>
+            </label>
+          </fieldset>
+
+          <fieldset class="form__block">
+            <legend class="form__legend">Цвет</legend>
+            <ul class="colors">
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input
+                    class="colors__radio sr-only"
+                    type="radio"
+                    name="color"
+                    value="#73B6EA"
+                    checked=""
+                  />
+                  <span class="colors__value" style="background-color: #73b6ea"> </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FFBE15" />
+                  <span class="colors__value" style="background-color: #ffbe15"> </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#939393" />
+                  <span class="colors__value" style="background-color: #939393"> </span
+                ></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#8BE000" />
+                  <span class="colors__value" style="background-color: #8be000"> </span
+                ></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FF6B00" />
+                  <span class="colors__value" style="background-color: #ff6b00"> </span
+                ></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#FFF" />
+                  <span class="colors__value" style="background-color: #fff"> </span
+                ></label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" name="color" value="#000" />
+                  <span class="colors__value" style="background-color: #000"> </span
+                ></label>
+              </li>
+            </ul>
+          </fieldset>
+
+          <fieldset class="form__block">
+            <legend class="form__legend">Объемб в ГБ</legend>
+            <ul class="check-list">
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="8"
+                    checked=""
+                  />
+                  <span class="check-list__desc">
+                    8
+                    <span>(313)</span>
+                  </span>
+                </label>
+              </li>
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="16"
+                  />
+                  <span class="check-list__desc">
+                    16
+                    <span>(461)</span>
+                  </span>
+                </label>
+              </li>
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="32"
+                  />
+                  <span class="check-list__desc">
+                    32
+                    <span>(313)</span>
+                  </span>
+                </label>
+              </li>
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="64"
+                  />
+                  <span class="check-list__desc">
+                    64
+                    <span>(313)</span>
+                  </span>
+                </label>
+              </li>
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="128"
+                  />
+                  <span class="check-list__desc">
+                    128
+                    <span>(313)</span>
+                  </span>
+                </label>
+              </li>
+              <li class="check-list__item">
+                <label class="check-list__label">
+                  <input
+                    class="check-list__check sr-only"
+                    type="checkbox"
+                    name="volume"
+                    value="264"
+                  />
+                  <span class="check-list__desc">
+                    264
+                    <span>(313)</span>
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </fieldset>
+
+          <button class="filter__submit button button--primery" type="submit">Применить</button>
+          <button class="filter__reset button button--second" type="button">Сбросить</button>
+        </form>
+      </aside>
+
+      <section class="catalog">
+        <ProductList :products="products"></ProductList>
+        <BasePagination v-model:page="page" :count="countProducts" :per-page="productsPerPage">
+        </BasePagination>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import products from './data/products';
 import BasePagination from './components/BasePagination.vue';
+import ProductList from './components/ProductList.vue';
 
 export default {
   name: 'App',
-  components: { BasePagination },
+  components: { BasePagination, ProductList },
   data() {
     return {
-      publicPath: process.env.BASE_URL,
       page: 1,
       productsPerPage: 3,
     };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-// import { data } from './data/data';
-// import alertMessage from './data/alertMessage';
-
 createApp(App).mount('#app');
-
-// alertMessage(data.helloMessage);
-// alertMessage(data.startMessage);
