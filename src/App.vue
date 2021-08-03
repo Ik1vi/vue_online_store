92a0e8c4aa218f065c649ba6ad98d4429a142e29
Irene Veselova <ik1vikuz@gmail.com>
2021-08-03 22:59:41 +0300

    cookies block first time

--- src/App.vue
+++ src/App.vue
@@ -33,18 +33,24 @@ export default {
   created() {
     const accessKey = this.getCookie('userAccessKey');
     const rejectData = localStorage.getItem('rejectData');
-
-    if (accessKey) {
-      this.updateUserAccessKey(accessKey);
-      this.isCookieBlock = false;
-    } else if (rejectData) {
-      this.getAccessKey();
-      if (rejectData !== new Date().toLocaleDateString()) {
-        this.isCookieBlock = true;
-      } else if (rejectData === new Date().toLocaleDateString()) {
+    const cookieBlockFirstTime = localStorage.getItem('cookieBlockFirstTime');
+    if (cookieBlockFirstTime) {
+      if (accessKey) {
+        this.updateUserAccessKey(accessKey);
         this.isCookieBlock = false;
+      } else if (rejectData) {
+        this.getAccessKey();
+        if (rejectData !== new Date().toLocaleDateString()) {
+          this.isCookieBlock = true;
+        } else if (rejectData === new Date().toLocaleDateString()) {
+          this.isCookieBlock = false;
+        }
+      } else {
+        this.getAccessKey();
       }
     } else {
+      localStorage.setItem('cookieBlockFirstTime', 'true');
+      this.isCookieBlock = true;
       this.getAccessKey();
     }
     this.loadCart();
